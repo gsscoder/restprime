f9a4ebf002e017285eea6004cc0ccf35b2cbe70e
Giacomo Stelluti Scala <gsscoder@gmail.com>
2025-03-24 19:46:34 +0100

    Drafted AppController base class

---
+++ src/RestPrime/AppController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestPrime.Models;
+
+namespace RestPrime;
+
+public abstract class AppController : ControllerBase
+{
+    #region Results helpers
+    protected static IActionResult BadRequest(string message, string? detail = null) =>
+        new ObjectResult(new ErrorResult
+        {
+            Type = ErrorResult.Error400BadRequest,
+            Title = message,
+            Status = StatusCodes.Status400BadRequest,
+            Detail = detail,
+        })
+        {
+            StatusCode = StatusCodes.Status400BadRequest
+        };
+
+    protected static IActionResult Unauthorized(string message) =>
+        new ObjectResult(new ErrorResult
+        {
+            Type = ErrorResult.Error401Unauthorized,
+            Title = "Unauthorized",
+            Status = StatusCodes.Status401Unauthorized,
+            Detail = message,
+        })
+        {
+            StatusCode = StatusCodes.Status401Unauthorized
+        };
+    #endregion
+}
---
+++ src/RestPrime/Models/ErrorResult.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+
+namespace RestPrime.Models;
+
+/// <summary>ASP.NET pipeline default error mapping based on https://datatracker.ietf.org/doc/html/rfc7807</summary>
+public sealed class ErrorResult
+{
+    public const string Error400BadRequest = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1";
+    public const string Error401Unauthorized = "https://datatracker.ietf.org/doc/html/rfc7235#section-3.1";
+    public const string Error403Forbidden = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.3";
+    public const string Error404NotFound = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4";
+    public const string Error409Conflict = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.8";
+    public const string Error412PreconditionFailed = "https://datatracker.ietf.org/doc/html/rfc7232#section-4.2";
+    public const string Error500InternalServerError = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1";
+    public const string Error502BadGatewayError = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.3";
+    static readonly Dictionary<int, string> _errorMap = new() {
+        { 400, Error400BadRequest },
+        { 401, Error401Unauthorized },
+        { 403, Error403Forbidden },
+        { 404, Error404NotFound },
+        { 404, Error404NotFound },
+        { 409, Error409Conflict },
+        { 412, Error412PreconditionFailed },
+        { 500, Error500InternalServerError },
+        { 502, Error502BadGatewayError },
+    };
+
+    public ErrorResult()
+    {
+    }
+
+    public ErrorResult(int statusCode)
+    {
+        if (!Enum.IsDefined(typeof(HttpStatusCode), statusCode)) {
+            throw new ArgumentOutOfRangeException(nameof(statusCode), statusCode, "Invalid status code");
+        }
+
+        Type = _errorMap.GetValueOrDefault(statusCode);
+        Status = statusCode;
+    }
+
+    public string? Type { get; set; }
+
+    public string? Title { get; set; }
+
+    public int? Status { get; set; }
+
+    public string? Detail { get; set; }
+
+    public string? TraceId { get; set; }
+
+    public List<object> Errors { get; set; } = null!;
+}
